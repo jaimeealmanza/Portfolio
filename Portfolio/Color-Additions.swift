5d464157b59759ee2085ecc2e782839c00708890
Jaime E. Almanza <almanza.jaime@icloud.com>
2020-11-23 19:21:25 -0700

    Built the HomeView to show open projects and upcoming Items

---
+++ Portfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+// 
+// Color-Additions.swift of Portfolio.
+// Created by JaimeAlmanza on 23/11/20.
+//
+// 
+
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(.secondarySystemGroupedBackground)
+}

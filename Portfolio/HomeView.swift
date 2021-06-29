113088fced9a4c7cfc19d4391e2f8028b299be69
Jaime E. Almanza <almanza.jaime@icloud.com>
2021-06-29 17:40:47 -0600

    Modified deletion rules for the project, and updated CoreData predicate

--- Portfolio/HomeView.swift
+++ Portfolio/HomeView.swift
@@ -9,51 +9,60 @@ import SwiftUI
 import CoreData
 
 struct HomeView: View {
-    static let tag: String? = "Home"
-    @EnvironmentObject var dataController: DataController
-    
-    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
-    let items: FetchRequest<Item>
-    
-    var projectRows: [GridItem] {
-        [GridItem(.fixed(100))]
-    }
-    
-    init() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "completed = false")
-        
-        request.sortDescriptors = [
-            NSSortDescriptor(keyPath: \Item.priority, ascending: false)
-        ]
-        
-        request.fetchLimit = 10
-        items = FetchRequest(fetchRequest: request)
-    }
-    
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHGrid(rows: projectRows) {
-													ForEach(projects, content: ProjectSummaryView.init)
-                        }
-                        .padding([.horizontal, .top])
-                        .fixedSize(horizontal: false, vertical: true)
-                    }
-                    
-                    VStack(alignment: .leading) {
-											ItemListView(title: "Up next", items: items.wrappedValue.prefix(3))
-											ItemListView(title: "More to explore", items: items.wrappedValue.dropFirst(3))
-                    }
-                    .padding(.horizontal)
-                }
-            }
-            .background(Color.systemGroupedBackground.ignoresSafeArea())
-            .navigationTitle("Home")
-        }
-    }
+	static let tag: String? = "Home"
+	@EnvironmentObject var dataController: DataController
+	
+	@FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+	let items: FetchRequest<Item>
+	
+	var projectRows: [GridItem] {
+		[GridItem(.fixed(100))]
+	}
+	
+	init() {
+		let request: NSFetchRequest<Item> = Item.fetchRequest()
+		let completedPredicate = NSPredicate(format: "completed = false")
+		let openPredicate = NSPredicate(format: "project.closed = false")
+		let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [completedPredicate, openPredicate])
+		request.predicate = compoundPredicate
+		
+		request.sortDescriptors = [
+			NSSortDescriptor(keyPath: \Item.priority, ascending: false)
+		]
+		
+		request.fetchLimit = 10
+		items = FetchRequest(fetchRequest: request)
+	}
+	
+	var body: some View {
+		NavigationView {
+			ScrollView {
+				VStack(alignment: .leading) {
+					ScrollView(.horizontal, showsIndicators: false) {
+						LazyHGrid(rows: projectRows) {
+							ForEach(projects, content: ProjectSummaryView.init)
+						}
+						.padding([.horizontal, .top])
+						.fixedSize(horizontal: false, vertical: true)
+					}
+					
+					VStack(alignment: .leading) {
+						ItemListView(title: "Up next", items: items.wrappedValue.prefix(3))
+						ItemListView(title: "More to explore", items: items.wrappedValue.dropFirst(3))
+					}
+					.padding(.horizontal)
+				}
+			}
+			.background(Color.systemGroupedBackground.ignoresSafeArea())
+			.navigationTitle("Home")
+			.toolbar {
+				Button("Add Data") {
+					dataController.deleteAll()
+					try? dataController.createSampleData()
+				}
+			}
+		}
+	}
 }
 
 //Button("Add Data") {
@@ -62,7 +71,7 @@ struct HomeView: View {
 //}
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+	static var previews: some View {
+		HomeView()
+	}
 }
